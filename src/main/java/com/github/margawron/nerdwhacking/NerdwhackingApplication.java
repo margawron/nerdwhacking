36610d056443849f6a105f9ce6a87ed38c468c2f
Marcel Gawron <marcelgawron@outlook.com>
2024-04-15 19:58:23 +0200

    Repo init.

---
+++ src/main/java/com/github/margawron/nerdwhacking/NerdwhackingApplication.java
@@ -0,0 +1,13 @@
+package com.github.margawron.nerdwhacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdwhackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdwhackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/margawron/nerdwhacking/NerdwhackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.margawron.nerdwhacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdwhackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
